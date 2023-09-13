725e0421107c6b1f0f073dfb7a5b93f83a865749
Theo <kanbikil@gmail.com>
2023-09-13 17:14:18 +0200

    AFPA

--- SQL/exo/Requete serie 5.sql
+++ SQL/exo/Requete serie 5.sql
@@ -11,3 +11,19 @@ select
 	*
 from station 
 where altitude < 1000;
+
+select 
+	number,capacity 
+from room r 
+where capacity > 1 ;
+
+select 
+	last_name,
+	city 
+from client c 
+where city not like 'Londres' ;
+
+select
+	name, city, category
+from hotel h 
+where city like 'Bretou' and category > 3 ;
