30548694f2ca088c5eb8991fc941c9ce5b8636a7
Theo <kanbikil@gmail.com>
2023-09-20 13:06:16 +0200

    afpa

---
+++ JS/memory/script.js
@@ -0,0 +1,33 @@
+/* Initialisation */
+var lesCartes = document.querySelectorAll('.image');
+console.log(lesCartes);
+;
+/* Nombre de Joueurs */
+var nbrJoueur = document.getElementById('player');
+
+/* Nombre de Pairs */
+var nbrPair = document.getElementById('pairs');
+
+/* Chrono */
+var leChrono = document.getElementById('chrono');
+console.log(leChrono);
+
+/* CardGeneration */
+
+
+/* function Shuffle */
+
+
+/* Starting game */
+
+
+/* function FlipCard */
+
+
+/* function Checking */
+
+
+/* function CheckOk */
+
+
+/* function Victory */
