e0afe3b7c19ed98df5a5f8b39527a71501a340f3
florentdevk <138789832+florentdevk@users.noreply.github.com>
2023-10-10 20:32:53 +0200

    modif flo

--- phpscript/attributes.php
+++ phpscript/attributes.php
@@ -43,33 +43,70 @@ function generateattributes($table,$colonnes)
     $return.= "     return self::" . '$_attributes' . ";" . "\n";
     $return.= "}". "\n" ;
     //Partie de Florent 
-    // Construct
-    $return.= "\n" . "\n";
-    $return.= " /******************Construct*******************/" . "\n" . "\n";
-    $return.= "public function __construct(array \$options = [])\n";
-    $return.= "{\n";
-    $return.= "    if (!empty(\$options))\n";
-    $return.= "    {\n";
-    $return.= "        \$this->hydrate(\$options);\n";
-    $return.= "    }\n";
-    $return.= "}\n";
-    // Hydrate
-    $return.= "public function hydrate(\$data)\n";
-    $return.= "{\n";
-    $return.= "    foreach (\$data as \$key => \$value) {\n";
-    $return.= "        \$method = \"set\" . ucfirst(\$key);\n";
-    $return.= "        if (is_callable([\$this, \$method]))\n";
-    $return.= "        {\n";
-    $return.= "            \$this->\$method(\$value);\n";
-    $return.= "        }\n";
-    $return.= "    }\n";
-    $return.= "}\n";
-    // fonction to string
-    $return.= "public function toString()\n";
-    $return.= "{\n";
+    
+/**
+ * Permet de créer la fonction construct
+ *
+ * @return void
+ */
+function generateConstruct()
+{
+    $aff = '';
 
-    $return.= "}\n";
-    return $return;
+    $aff .= "public function __construct(array \$options = [])\n";
+    $aff .= "{\n";
+    $aff .= "    if (!empty(\$options))\n";
+    $aff .= "    {\n";
+    $aff .= "        \$this->hydrate(\$options);\n";
+    $aff .= "    }\n";
+    $aff .= "}\n";
+
+    return $aff;
+}
+
+/**
+ * Permet de créer la fonction hydrate
+ *
+ * @return void
+ */
+function generateHydrate()
+{
+    $aff = '';
+
+    $aff .= "public function hydrate(\$data)\n";
+    $aff .= "{\n";
+    $aff .= "    foreach (\$data as \$key => \$value) {\n";
+    $aff .= "        \$method = \"set\" . ucfirst(\$key);\n";
+    $aff .= "        if (is_callable([\$this, \$method]))\n";
+    $aff .= "        {\n";
+    $aff .= "            \$this->\$method(\$value);\n";
+    $aff .= "        }\n";
+    $aff .= "    }\n";
+    $aff .= "}\n";
+
+    return $aff;
+}
+
+/**
+ * Transforme l'objet en chaine de caractères
+ *
+ * @return String
+ */
+function generateToString()
+{
+    $aff = '';
+
+    $aff .= "public function toString()\n";
+    $aff .= "{\n";
+    $aff .= "    \$properties = get_object_vars(\$this);\n";
+    $aff .= "    \$output = [];\n";
+    $aff .= "    foreach (\$properties as \$name => \$value) {\n";
+    $aff .= "        \$output[] = \"\$name : \$value\";\n";
+    $aff .= "    }\n";
+    $aff .= "    return implode(\", \", \$output);\n";
+    $aff .= "}\n";
+
+    return $aff;
 }
 
 // generateattributes("toto", ["nom","prenom"]);
