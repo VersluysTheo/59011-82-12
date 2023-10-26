34270fdb7b76ff494b8cc52f9a4f1668274cebde
Theo Versluys <kanbikil@gmail.com>
2023-10-26 17:13:51 +0200

    c#

---
+++ CSH/Test2/Test2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char var = 'a' ;
+            Console.WriteLine(var);
+        }
+    }
+}
+
+//1.1
+// Exo1
+//Console.WriteLine("Votre Nom ?");
+//string s = Console.ReadLine();
+//Console.WriteLine("Bonjour " + s);
+
+// Exo 2
+//Console.Write("Votre Age ?");
+//string valeur = Console.ReadLine();
+//int convert = Convert.ToInt32(valeur);
+//Console.WriteLine("Vous avez " + convert + " ans");
+
+//1.2
+// Exo 3
+//Console.WriteLine("a = ?");
+//string valeur1 = Console.ReadLine();
+//int a = Convert.ToInt32(valeur1);
+//Console.WriteLine("b = ?");
+//string valeur2 = Console.ReadLine();
+//int b = Convert.ToInt32(valeur2);
+//Console.WriteLine("Somme : " + (a + b) + " Quotient : " + (a / b));
+
+//1.3
+//Exo4 
+//Console.Write("Ecrit une valeur flottante : ");
+//string valeur = Console.ReadLine();
+//decimal convert = Convert.ToDecimal(valeur);
+//Console.WriteLine(convert);
+
+//Exo5
+//Console.Write("Valeur 1 ? ");
+//string valeur1 = Console.ReadLine();
+//decimal convert = Convert.ToDecimal(valeur1);
+//Console.Write("Valeur 2 ? ");
+//string valeur2 = Console.ReadLine();
+//decimal convert2 = Convert.ToDecimal(valeur2);
+//Console.Write("Valeur 3 ? ");
+//string valeur3 = Console.ReadLine();
+//decimal convert3 = Convert.ToDecimal(valeur3);
+//Console.WriteLine("Moyenne : " +(convert + convert2 + convert3) / 3);
+
+//Exo6
+//Console.Write("Longueur du Rectangle ? ");
+//string valeur = Console.ReadLine();
+//int longueur = Convert.ToInt32(valeur);
+//Console.Write("Largeur du Rectangle ? ");
+//string valeur1 = Console.ReadLine();
+//int largeur = Convert.ToInt32(valeur1);
+//Console.WriteLine("Aire du Rectangle : " + (longueur * largeur));
+
+//1.4
+//Exo 7
+
+
+//Exo 8 
+//Console.WriteLine("Ecrivez un texte en minuscule : ");
+//string s = Console.ReadLine();
+//Console.WriteLine(s.ToUpper());
