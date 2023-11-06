becdc630738aef1a4f388cf5b7f449bd58d19df7
Theo Versluys <kanbikil@gmail.com>
2023-11-06 16:16:29 +0100

    c# POO exemple voiture

--- CSH/Voiture/Voiture/Program.cs
+++ CSH/Voiture/Voiture/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+static void Main(string[] args)
+{
+    //Voiture voiture1 = new()
+    //{
+    //    Couleur = "Bleu",
+    //    Marque = "Citröen",
+    //    Modele = "C4",
+    //    Nb_de_Kilometre = 10000,
+    //    Motorisation = "Diesel"
+    //};
+    //Voiture voiture2 = new()
+    //{
+    //    Couleur = "Rouge",
+    //    Marque = "Renault",
+    //    Modele = "Kadjar",
+    //    Nb_de_Kilometre = 16523,
+    //    Motorisation = "Essence"
+    //};
+    //voiture1.Mavoiture();
+    }
