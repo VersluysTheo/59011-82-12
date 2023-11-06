69740905c4d957da3e2197bc7ed5b313148d2007
Theo Versluys <kanbikil@gmail.com>
2023-11-06 16:49:45 +0100

    c#class poo

--- CSH/Voiture/Voiture/Class1.cs
+++ CSH/Voiture/Voiture/Class1.cs
@@ -8,5 +8,50 @@ namespace Voiture
 {
     internal class Voiture
     {
+
+        // Propriétés
+        public string Couleur { get; set; }
+        public string Marque { get; set; }
+        public string Modele { get; set; }
+        public int Nb_de_kilometre { get; set; }
+        public string Motorisation { get; set; }
+
+        //Differents Constructeurs
+
+        public Voiture(string couleur, string marque, string modele, int nb_de_kilometre, string motorisation)
+        {
+            this.Couleur = couleur;
+            this.Marque = marque;
+            this.Modele = modele;
+            this.Nb_de_kilometre = nb_de_kilometre;
+            this.Motorisation = motorisation;
+        }
+
+
+        // Getter et Setter Methode 1
+        //public string Couleur { get => couleur; set couleur = value; }
+
+        // Getter et Setter Methode 2
+
+        //public string GetCouleurVoiture()
+        //{
+        //    return Couleur;
+        //}
+
+        //public void SetCouleurVoiture(string value)
+        //{
+        //    Couleur = value;
+        //}
+
+        public override string ToString()
+        {
+            return "Cette voiture est une " + Modele + " de la marque " + Marque + ",de couleur " + Couleur + ",de motorisation " + Motorisation + ",avec " + Nb_de_kilometre + " Kilomètres";
+        }
+
+        public int Rouler(int nbkmroule)
+        {
+            return Nb_de_kilometre += nbkmroule;
+        }
+
     }
 }
--- CSH/Voiture/Voiture/Program.cs
+++ CSH/Voiture/Voiture/Program.cs
@@ -1,22 +1,16 @@
-﻿using System.Reflection;
-
-static void Main(string[] args)
+﻿namespace Voiture
 {
-    //Voiture voiture1 = new()
-    //{
-    //    Couleur = "Bleu",
-    //    Marque = "Citröen",
-    //    Modele = "C4",
-    //    Nb_de_Kilometre = 10000,
-    //    Motorisation = "Diesel"
-    //};
-    //Voiture voiture2 = new()
-    //{
-    //    Couleur = "Rouge",
-    //    Marque = "Renault",
-    //    Modele = "Kadjar",
-    //    Nb_de_Kilometre = 16523,
-    //    Motorisation = "Essence"
-    //};
-    //voiture1.Mavoiture();
+
+    class Wouhou
+    {
+        static void Main()
+        {
+            Voiture v1 = new Voiture("Noir", "Citröen" , "C4", 100000, " Essence");
+            Voiture v2 = new Voiture("Rouge", "Renault", "Kadjar", 251656, " Diesel");
+            Console.WriteLine(v1.ToString());
+            Console.WriteLine(v2);
+            v1.Rouler(289);
+            Console.WriteLine(v1);
+        }
     }
+}
