efb2875138bfffdcf852c39fcd228d2c7da82968
Theo Versluys <kanbikil@gmail.com>
2023-11-07 15:55:06 +0100

    c# POO

--- CSH/Employe/Employe/Class1.cs
+++ CSH/Employe/Employe/Class1.cs
@@ -28,20 +28,35 @@ namespace Employe
             Service = service;
         }
 
+
         public void Recruterdepuis()
         {
             int embauche = DateTime.Now.Year - Date_Embauche.Year;
-            if ((Date_Embauche.Month > DateTime.Now.Month) || (Date_Embauche.Day > DateTime.Now.Day))
+            if (Date_Embauche.Month > DateTime.Now.Month)
             {
                 embauche--;
             }
-            Console.WriteLine(Prenom + " " + Nom + " a été recruté il y a " + embauche + " ans");
+            Console.WriteLine(Prenom + " " + Nom + " a été recruté il y a " + embauche + " ans \n");
         }
 
         public void Prime(double annee)
         {
-            double montant = Salaire * (0.05*annee);
-            Console.WriteLine("Pour ses " + annee + " années d'ancienneté il touchera " + montant + " euros de prime");
+            double montant = Math.Round(Salaire * (0.05+(0.02*annee)),2 );
+            Console.WriteLine("Pour ses " + annee + " années d'ancienneté il touchera " + montant + " k euros de prime \n");
+        }
+
+        public void CheckPrime()
+        {
+            DateTime today = DateTime.Now;
+            DateTime jourprime = new DateTime(2023, 11, 08);
+            if ((today.Month == jourprime.Month) && (today.Day == jourprime.Day))
+            {
+                Console.WriteLine("Le Versement de la prime a été effectué \n");
+            } else
+            {
+                Console.WriteLine("Le Versement est prévu pour tout les 7 Octobre de chaque année \n");
+            }
+
         }
 
         public void Afficher()
@@ -49,7 +64,7 @@ namespace Employe
             Console.WriteLine("Employe : " + Nom + " " + Prenom );
             Console.WriteLine("Date d'embauche : " + Date_Embauche);
             Console.WriteLine("Employe en tant que " + Fonction + " dans le service " + Service);
-            Console.WriteLine("Salaire : " + Salaire + "k euros brut par an");
+            Console.WriteLine("Salaire : " + Salaire + "k euros brut par an \n");
         }
     }
 }
--- CSH/Employe/Employe/Program.cs
+++ CSH/Employe/Employe/Program.cs
@@ -5,9 +5,20 @@
         static void Main()
         {
             Employee e1 = new("Nolk", "Paul", new DateTime(2021,11,14), "Assistant Réseau", 28, "Informatique");
+            Employee e2 = new("Chirac", "Jacques", new DateTime(1980,06,21), "Directeur général",55,"Ressources Humaines");
+            Employee e3 = new("Moka", "Gerard", new DateTime(2018, 03, 17), "Comptable", 37, "Comptabilité");
+            Employee e4 = new("Jun", "Ling", new DateTime(1994, 10, 01), "Secretaire", 40, "Secrétariat");
+            Employee e5 = new("Evrard", "Marjorie", new DateTime(1989, 05, 26), "Chargée de Recrutement", 32, "Ressources Humaines");
+            Employee e6 = new("Brio", "Maxime", new DateTime(1995, 02, 13), "Chargé de Projet", 30, "Informatique");
             e1.Afficher();
-            e1.Recruterdepuis();
+            e6.Recruterdepuis();
             e1.Prime(2);
+            e2.Prime(42);
+            e3.Prime(4);
+            e4.Prime(29);
+            e5.Prime(33);
+            e6.Prime(27);
+            e1.CheckPrime();
         }
     }
 }
