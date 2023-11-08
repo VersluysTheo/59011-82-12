8e072f9503ec8644093324e21c2ad3d809fa0471
Theo Versluys <kanbikil@gmail.com>
2023-11-08 16:58:40 +0100

    c# POO

---
+++ CSH/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Rectangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rectangle rectangle1 = new(4, 5);
+            rectangle1.AfficherRectangle();
+        }
+    }
+}
---
+++ CSH/Rectangle/Rectangle/TriangleRectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class TriangleRectangle
+    {
+        public int Base {  get; set; }
+        public int Hauteur { get; set; }
+
+        public TriangleRectangle(int @base, int hauteur)
+        {
+            Base = @base;
+            Hauteur = hauteur;
+        }
+    }
+}
