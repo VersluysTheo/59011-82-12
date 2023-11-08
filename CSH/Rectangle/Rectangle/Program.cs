a994ae7625eae426b77987ea01742acde4df5867
Theo Versluys <kanbikil@gmail.com>
2023-11-08 17:13:35 +0100

    c#POO

--- CSH/Rectangle/Rectangle/Program.cs
+++ CSH/Rectangle/Rectangle/Program.cs
@@ -6,6 +6,8 @@
         {
             Rectangle rectangle1 = new(4, 5);
             rectangle1.AfficherRectangle();
+            Triangle triangle1 = new(8, 10);
+            triangle1.AfficherTriangle();
         }
     }
 }
---
+++ CSH/Rectangle/Rectangle/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Triangle
+    {
+        public double Base {  get; set; }
+        public double Hauteur { get; set; }
+
+        public Triangle(double @base, double hauteur)
+        {
+            Base = @base;
+            Hauteur = hauteur;
+        }
+
+        public double Perimetre()
+        {
+            double perimetre;
+            perimetre = Math.Round(Base + Hauteur + Math.Sqrt(Math.Pow(Base,2)+Math.Pow(Hauteur,2)),2);
+            return perimetre;
+        }
+
+        public void AfficherTriangle()
+        {
+            Console.WriteLine($"{Perimetre()}");
+        }
+    }
+}
