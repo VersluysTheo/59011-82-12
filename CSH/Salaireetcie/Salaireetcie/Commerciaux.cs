58f7c34ab7023f98ba5b5b365eac218076b3b5fb
Theo Versluys <kanbikil@gmail.com>
2023-11-09 15:47:35 +0100

    c# POO

---
+++ CSH/Salaireetcie/Salaireetcie/Commerciaux.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salaireetcie
+{
+    public class Commerciaux : Employe
+    {
+        public int NbDeplacement { get; set; }
+        public int NbPrime { get; set; }
+
+        public Commerciaux(string nom, string prenom, int age, double salaire, int nbDeplacement, int nbPrime) : base(nom,prenom,age,salaire)
+    {
+        NbDeplacement = nbDeplacement;
+        NbPrime = nbPrime;
+    }
+
+    }
+}
---
+++ CSH/Salaireetcie/Salaireetcie/Entreprise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salaireetcie
+{
+    public class Entreprise
+    {
+        public string Nom {  get; set; }
+        public List<Commerciaux> ListeCommerciaux { get; set; }
+        public List<Technicien> ListeTechniciens { get; set; }
+
+        public Entreprise(string nom, List<Commerciaux> listeCommerciaux, List<Technicien> listeTechniciens)
+        {
+            Nom = nom;
+            ListeCommerciaux = listeCommerciaux;
+            ListeTechniciens = listeTechniciens;
+        }
+    }
+}
