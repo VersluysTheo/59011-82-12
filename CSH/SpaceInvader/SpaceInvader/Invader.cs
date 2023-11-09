965d5ca9964225ba60abb870d5d6d40055895725
Theo Versluys <kanbikil@gmail.com>
2023-11-09 16:47:41 +0100

    SpaceInvader c#

---
+++ CSH/SpaceInvader/SpaceInvader/Invader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvader
+{
+    public class Invader
+    {
+        public char Inv {  get; set; }
+
+        public Invader()
+        {
+            Inv = '#';
+        }
+
+        public override string ToString()
+        {
+            return $"{Inv}";
+        }
+        public void AfficherInvader()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
---
+++ CSH/SpaceInvader/SpaceInvader/Space.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvader
+{
+    public class Space
+    {
+        // Propriétés
+        public int nbLignes { get; set; }
+        public int nbColonnes { get; set; }
+        public char[,] Grille { get; set; }
+
+        // Constructeur
+
+        public Space(int nbLignes, int nbColonnes)
+        {
+            this.nbLignes = nbLignes;
+            this.nbColonnes = nbColonnes;
+            Grille = InitGrille();
+        }
+        
+        public char[,] InitGrille()
+        {
+            char[,] Grille = new char[nbLignes,nbColonnes];
+            for (int i = 0; i < nbLignes; i++)
+            {
+                for (int j = 0 ; j < nbColonnes; j++)
+                {
+                    Grille[i,j] = ' ';
+                }
+            }
+            return Grille;
+        }
+
+        public override string ToString()
+        {
+            string s;
+            s = "";
+            for (int i = 0; i < Grille.GetLength(0); i++)
+            {
+                s+= "|";
+                for (int j = 0; j < Grille.GetLength(1); j++)
+                {
+                    s += Grille[i, j] ;
+                }
+                s += "|\n" ;
+            }
+            return s;
+
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
