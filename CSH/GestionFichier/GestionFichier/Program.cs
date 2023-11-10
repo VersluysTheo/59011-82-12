4c4181f910ebe3113d2720c2b55a2f06e467c3a4
Theo Versluys <kanbikil@gmail.com>
2023-11-10 11:32:55 +0100

    c# POO

---
+++ CSH/GestionFichier/GestionFichier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace GestionFichier
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] lines = { "Dakor", "Je comprend" };
+            Contenu.Ecrire("Bonjour",lines);
+        }
+    }
+}
