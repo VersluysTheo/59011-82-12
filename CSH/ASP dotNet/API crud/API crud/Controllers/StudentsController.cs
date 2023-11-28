52b79d70a16014714069a4a2dff07eab0cfc54eb
Theo Versluys <kanbikil@gmail.com>
2023-11-28 10:12:44 +0100

    api relation asp dot net

--- CSH/ASP dotNet/API crud/API crud/Controllers/StudentsController.cs
+++ CSH/ASP dotNet/API crud/API crud/Controllers/StudentsController.cs
@@ -1,7 +1,9 @@
-﻿using API_crud.Models.Dtos;
+﻿using API_crud.Models.Data;
+using API_crud.Models.Dtos;
 using API_crud.Models.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
@@ -25,6 +27,78 @@ namespace API_crud.Controllers
         {
             var listeStudents = _service.GetAllStudents();
             return Ok(_mapper.Map<IEnumerable<StudentsDTO>>(listeStudents));
+        }
+
+        // GET by id
+        [HttpGet("{id}", Name = "GetStudentById")]
+        public ActionResult<StudentsDTO> GetStudentById(int id)
+        {
+            var commandItem = _service.GetStudentById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<StudentsDTO>(commandItem));
+            }
+            return NotFound();
+        }
+
+        //POST (Add)
+        [HttpPost]
+        public ActionResult<StudentsDTO> CreateStudent(Student student)
+        {
+            //on ajoute l’objet à la base de données
+            _service.AddStudent(student);
+            //on retourne le chemin de findById avec l'objet créé
+            return CreatedAtRoute(nameof(GetStudentById), new
+            {
+                student.Id
+            }, student);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateStudent(int id, StudentsDTO student)
+        {
+            var studentFromRepo = _service.GetStudentById(id);
+            if (studentFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(student, studentFromRepo);
+            _service.UpdateStudent(studentFromRepo);
+            return NoContent();
+        }
+
+        //PATCH api/students/{id}
+        [HttpPatch("{id}")]
+        public ActionResult PartialStudentUpdate(int id, JsonPatchDocument<Student>
+       patchDoc)
+        {
+            var studentFromRepo = _service.GetStudentById(id);
+            if (studentFromRepo == null)
+            {
+                return NotFound();
+            }
+            var studentToPatch = _mapper.Map<Student>(studentFromRepo);
+            patchDoc.ApplyTo(studentToPatch, ModelState);
+            if (!TryValidateModel(studentToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+            _mapper.Map(studentToPatch, studentFromRepo);
+            _service.UpdateStudent(studentFromRepo);
+            return NoContent();
+        }
+
+        //DELETE api/students/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteStudent(int id)
+        {
+            var studentModelFromRepo = _service.GetStudentById(id);
+            if (studentModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _service.DeleteStudent(studentModelFromRepo);
+            return NoContent();
         }
 
     }
--- CSH/ASP dotNet/API crud/API crud/Program.cs
+++ CSH/ASP dotNet/API crud/API crud/Program.cs
@@ -1,6 +1,6 @@
-
-
+using API_crud.Models;
 using API_crud.Models.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_crud
 {
@@ -10,15 +10,17 @@ namespace API_crud
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<ApicrudContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("Default")));
+
             // Add services to the container.
             builder.Services.AddTransient<StudentsServices>();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            builder.Services.AddControllers().AddNewtonsoftJson();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddControllers().AddNewtonsoftJson();
 
             var app = builder.Build();
 
