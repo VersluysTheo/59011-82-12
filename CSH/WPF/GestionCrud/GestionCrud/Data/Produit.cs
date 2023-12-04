29778c661cb4c45e4ce7ffc157d745e599103f5e
Theo Versluys <kanbikil@gmail.com>
2023-12-04 16:40:10 +0100

    Creation de la classe produit et du fichier json

---
+++ CSH/WPF/GestionCrud/GestionCrud/Data/Produit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCrud.Data
+{
+    internal class Produit
+    {
+        public string? LibelleProduit {  get; set; }
+        public string? Description { get; set; }
+        public string? Categorie { get; set; }
+        public string? Stock { get; set; }
+        public int? Prix { get; set; }
+    }
+}
--- CSH/WPF/GestionCrud/GestionCrud/MainWindow.xaml.cs
+++ CSH/WPF/GestionCrud/GestionCrud/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Grpc.Core;
+using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +29,25 @@ namespace GestionCrud
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public DataTable jsonDataDisplay()
+        {
+            StreamReader sr = new StreamReader(Server.MapPath("./Json/produit.json"));
+            string json = sr.ReadToEnd();
+            dynamic table = JsonConvert.DeserializeObject(json);
+            DataTable dt = new DataTable();
+            dt.Columns.Add("LibelleProduit",typeof(string));
+            dt.Columns.Add("Description",typeof(string));
+            dt.Columns.Add("Categorie",typeof(string));
+            dt.Columns.Add("Stock",typeof(string));
+            dt.Columns.Add("Prix",typeof(int));
+
+            foreach (var row in table.value.data)
+            {
+                dt.Rows.Add(row.LibelleProduit, row.Description,row.Categorie,row.Stock,row.Prix);
+            }
+            return dt;
         }
     }
 }
