a913291b8572fc319e6870a80033c9702a917467
Theo Versluys <kanbikil@gmail.com>
2023-12-05 11:24:42 +0100

    Ajout des Evenements sur les boutons

---
+++ CSH/WPF/GestionCrud/GestionCrud/Controller/ProduitsController.cs
@@ -0,0 +1,67 @@
+﻿using GestionCrud.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GestionCrud.Controller
+{
+    class ProduitsController
+    {
+        private ObservableCollection<Produit> produitsList;
+        public ProduitsController(ObservableCollection<Produit> produitsList)
+        {
+            this.produitsList = produitsList;
+        }
+
+
+        public void LoadDataFromFile()
+        {
+            try
+            {
+                string filePath = "U:\\59011-82-12\\CSH\\WPF\\GestionCrud\\GestionCrud\\Json\\produit.json";
+                if (File.Exists(filePath))
+                {
+                    string jsonContent = File.ReadAllText(filePath);
+                    var produitsFromFile = JsonConvert.DeserializeObject<List<Produit>>(jsonContent);
+                    produitsList.Clear();
+
+                    foreach (var produit in produitsFromFile)
+                    {
+                        produitsList.Add(produit);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Le fichier JSON n'existe pas.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors du chargement des données : {ex.Message}");
+            }
+        }
+
+        public void SaveDataToFile()
+        {
+            try
+            {
+                string filePath = "U:\\59011-82-12\\CSH\\WPF\\GestionCrud\\GestionCrud\\Json\\produit.json";
+                string jsonContent = JsonConvert.SerializeObject(produitsList, Formatting.Indented);
+                File.WriteAllText(filePath, jsonContent);
+
+                MessageBox.Show("Données enregistrées avec succès !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur s'est produite lors de l'enregistrement des données : {ex.Message}");
+            }
+        }
+    }
+}
--- CSH/WPF/GestionCrud/GestionCrud/MainWindow.xaml.cs
+++ CSH/WPF/GestionCrud/GestionCrud/MainWindow.xaml.cs
@@ -1,23 +1,12 @@
-﻿using GestionCrud.Data;
-using MongoDB.Bson.IO;
+﻿using GestionCrud.Controller;
+using GestionCrud.Data;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Data;
+using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GestionCrud
 {
@@ -26,64 +15,34 @@ namespace GestionCrud
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Produit> produitsList;
+        private ProduitsController produitsController;
+
         public MainWindow()
         {
+            // Initialisation
             InitializeComponent();
-            // Exemple d'utilisation avec une classe Produit
-            GestionnaireJson<Produit> gestionnaireJson = new GestionnaireJson<Produit>("U:\\59011-82-12\\CSH\\WPF\\GestionCrud\\GestionCrud\\Json\\produit.json");
-
-            // Uploader des données
-            Produit produit = new Produit { /* initialisez les propriétés de votre produit ici */ };
-            gestionnaireJson.UploadData(produit);
-
-            // Télécharger des données
-            Produit produitTelecharge = gestionnaireJson.DownloadData();
+            produitsList = new ObservableCollection<Produit>();
+            produitsController = new ProduitsController(produitsList);
+            produitsController.LoadDataFromFile();
+            dtgProduit.ItemsSource = produitsList;
         }
 
-
-        public class GestionnaireJson<Produit>
+        // Gestion Evenement Bouton Delete
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            private readonly string cheminFichier;
-
-            public GestionnaireJson(string cheminFichier)
-            {
-                this.cheminFichier = cheminFichier;
-            }
-
-            public void UploadData(Produit data)
-            {
-                try
-                {
-                    string jsonData = JsonConvert.SerializeObject(data, Formatting.Indented);
-                    File.WriteAllText(cheminFichier, jsonData);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erreur lors de l'upload des données");
-                }
-            }
-
-            public Produit DownloadData()
-            {
-                try
-                {
-                    if (File.Exists(cheminFichier))
-                    {
-                        string jsonData = File.ReadAllText(cheminFichier);
-                        return JsonConvert.DeserializeObject<Produit>(jsonData);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Le fichier n'existe pas.");
-                        return default(Produit);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erreur lors du téléchargement des données");
-                    return default(Produit);
-                }
-            }
+                DeleteButton.IsEnabled = true;
         }
+        private void DataGrid_LostFocus(object sender, RoutedEventArgs e)
+        {
+            DeleteButton.IsEnabled = false;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AddButton.IsEnabled = true;
+            UpdateButton.IsEnabled = true;
+        }
+
     }
 }
