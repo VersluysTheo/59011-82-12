d0d33ff0ae27096cfde9625f3a0cff368e205819
Theo Versluys <kanbikil@gmail.com>
2023-12-06 16:06:53 +0100

    Creation Classe Personne

---
+++ CSH/WPF/ScaffoldCrud/ScaffoldCrud/Models/Data/Personne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaffoldCrud.Models.Data
+{
+    public partial class Personne
+    {
+        public int IdPersonne { get; set; }
+
+        public string Nom { get; set; } = null!;
+
+        public string Prenom { get; set; } = null!;
+
+        public string? Adresse { get; set; }
+
+        public string? Ville { get; set; }
+    }
+}
