b9dd1cac6539f975d25265cfd9fcd6ee122ac345
Theo Versluys <kanbikil@gmail.com>
2023-12-18 11:56:01 +0100

    Test unitaire

---
+++ CSH/CLI/TestUnitaire/Testprojetunitaire/UnitTest1.cs
@@ -0,0 +1,58 @@
+using System.Numerics;
+using TestUnitaire;
+
+namespace Testprojetunitaire
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Debit_MontantValide()
+        {
+            // Arrange
+            double soldeDepart = 11.99;
+            double montantDebite = 4.55;
+            double attendu = 7.44;
+            Compte compte = new Compte("Mr Toto", soldeDepart);
+            // Act
+            compte.Debit(montantDebite);
+            // Assert
+            double soldeActuel = compte.Solde;
+            Assert.AreEqual(attendu, soldeActuel, 0.001, "Compte mal débité");
+        }
+
+        [Test]
+        public void Debit_MontantNegatif()
+        {
+            // Arrange
+            double soldeDepart = 11.99;
+            double montantDebite = -4;
+            Compte compte = new Compte("Mr Toto", soldeDepart);
+            // Act et Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+           compte.Debit(montantDebite));
+        }
+
+        [Test]
+        public void Debit_MontantSuperieurSolde()
+        {
+            // Arrange
+            double soldeDepart = 11.99;
+            double montantDebite = -44.55;
+            Compte compte = new Compte("Mr Toto", soldeDepart);
+            // Act et Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+           compte.Debit(montantDebite));
+        }
+    }
+}
